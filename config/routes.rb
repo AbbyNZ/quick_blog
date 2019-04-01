d68f81a3f7e1bb7b67fd14db12e62cb7a238d83d
abby <bbatanes17@yahoo.com>
2019-04-01 15:43:09 +1000

    Custom error pages

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,8 @@ Rails.application.routes.draw do
   end
 
   get '/about' => 'pages#about'
+  get '/404' => 'errors#not_found'
+  get '/500' => 'errors#internal_error'
+  get '/422' => 'errors#unprocessable_entity'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
