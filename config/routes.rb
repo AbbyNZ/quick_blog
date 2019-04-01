d659681fcf3a9a3326f112fd66477a42dacfd87f
abby <bbatanes17@yahoo.com>
2019-04-01 14:58:30 +1000

    about me page implemented

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,12 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :users
-  root :to =>'posts#index'
+  root 'posts#index'
 
   resources :posts do
     resources :comments, only: [:create]
   end
+
+  get '/about' => 'pages#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
